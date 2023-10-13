d0369500333483d7e252610514a941c1afe020cb
yash-logistic <yash@logisticinfotech.co.in>
2023-10-13 13:17:46 +0530

    configured laravel vite and apply sass to page

---
+++ resources/views/site.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Main Page</title>
+    <meta name="viewport" content="width=device-width" />
+    @vite(['resources/scss/style.scss'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://kit.fontawesome.com/4d0b4c932e.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav>
+        <div class="nav-wrapper container">
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Visit</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="blog-container">
+        <div class="blog-post">
+            <img src="images/post-1.jpg" alt="Blog Post Image">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis justo vel neque facilisis, et
+                eleifend nunc vulputate. Sed sollicitudin, sapien non fermentum rhoncus, sem nisi eleifend odio, id
+                bibendum
+                ipsum metus ut nunc. Vivamus malesuada libero sit amet libero feugiat congue. Integer ac tellus vel
+                velit
+                tincidunt ullamcorper.</p>
+        </div>
+        <div class="blog-post">
+            <img src="images/post-2.jpg" alt="Blog Post Image">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis justo vel neque facilisis, et
+                eleifend nunc vulputate. Sed sollicitudin, sapien non fermentum rhoncus, sem nisi eleifend odio, id
+                bibendum
+                ipsum metus ut nunc. Vivamus malesuada libero sit amet libero feugiat congue. Integer ac tellus vel
+                velit
+                tincidunt ullamcorper.</p>
+        </div>
+        <div class="blog-post">
+            <img src="images/post-3.jpg" alt="Blog Post Image">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis justo vel neque facilisis, et
+                eleifend nunc vulputate. Sed sollicitudin, sapien non fermentum rhoncus, sem nisi eleifend odio, id
+                bibendum
+                ipsum metus ut nunc. Vivamus malesuada libero sit amet libero feugiat congue. Integer ac tellus vel
+                velit
+                tincidunt ullamcorper.</p>
+        </div>
+        <div class="blog-post">
+            <img src="images/post-1.jpg" alt="Blog Post Image">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis justo vel neque facilisis, et
+                eleifend nunc vulputate. Sed sollicitudin, sapien non fermentum rhoncus, sem nisi eleifend odio, id
+                bibendum
+                ipsum metus ut nunc. Vivamus malesuada libero sit amet libero feugiat congue. Integer ac tellus vel
+                velit
+                tincidunt ullamcorper.</p>
+        </div>
+        <div class="blog-post">
+            <img src="images/post-2.jpg" alt="Blog Post Image">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis justo vel neque facilisis, et
+                eleifend nunc vulputate. Sed sollicitudin, sapien non fermentum rhoncus, sem nisi eleifend odio, id
+                bibendum
+                ipsum metus ut nunc. Vivamus malesuada libero sit amet libero feugiat congue. Integer ac tellus vel
+                velit
+                tincidunt ullamcorper.</p>
+        </div>
+        <div class="blog-post">
+            <img src="images/post-3.jpg" alt="Blog Post Image">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis justo vel neque facilisis, et
+                eleifend nunc vulputate. Sed sollicitudin, sapien non fermentum rhoncus, sem nisi eleifend odio, id
+                bibendum
+                ipsum metus ut nunc. Vivamus malesuada libero sit amet libero feugiat congue. Integer ac tellus vel
+                velit
+                tincidunt ullamcorper.</p>
+        </div>
+    </div>
+    <footer class="page-footer teal">
+        <div class="container">
+            <div class="row">
+                <div class="col l3 s12">
+                    <h5 class="white-text">Web dev</h5>
+                    <p class="grey-text text-lighten-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+                        aspernatur at labore excepturi non praesentium voluptates quae corporis aperiam quod?</p>
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Join our newsletter</h5>
+                    <p class="grey-text text-lighten-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+                        sapiente accusantium porro minima ipsa ad!</p>
+                </div>
+                <div class="col l3">
+                    <h5 class="white-text">Social links</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Discord</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+                    </ul>
+                </div>
+                <div class="col l3">
+                    <h5 class="white-text">More</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Blog</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Privacy</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Terms</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Code of Conduct</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-copyright center">
+                <div class="container">
+                    © 2023 Shukla Yash
+                </div>
+            </div>
+    </footer>
+</body>
+
+</html>
